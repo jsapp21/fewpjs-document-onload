00e302175e1bec2115e98564f97858ef2589097f
James Sapp <jameseisapp@gmail.com>
2021-02-01 21:30:58 -0600

    commit tree c5d914efdb323442b6f33bcaf3acf033e38cb0ae

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let event  = document.getElementById("text")
+    event.textContent = "This is really cool!"
+  });
+  
+  
